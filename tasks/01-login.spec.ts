764de4b6f2ea4977e8f8b1f7f5ab8023b45c1170
sebadabrowski95 <sebadabrowski95@gmail.com>
2026-05-06 18:11:23 +0200

    test(01-login): Logs in with valid credentials and verifies if the user is actually logged in.
    The test validates the login process by monitoring the header state. It verifies that the default 'Log in/Sign in' link is replaced by the 'Log out/Sign out' button upon successful authentication.

--- tasks/01-login.spec.ts
+++ tasks/01-login.spec.ts
@@ -7,6 +7,32 @@ import { test, expect } from '@playwright/test'
  * Credentials: user@example.com / password12345
  */
 
+const VALID_EMAIL = 'user@example.com'
+const VALID_PASSWORD = 'password12345'
+
 test.describe('Login', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/login')
+  })
+
+  test('logs in with valid credentials', async ({ page }) => {
+    await expect(
+        page.locator('header')
+            .getByRole('button', { name: /log out|sign out/i })
+    ).not.toBeVisible();
+
+    await page.getByLabel('Email Address').fill(VALID_EMAIL)
+    await page.getByLabel('Password').fill(VALID_PASSWORD)
+    await page.click('[type="submit"]')
+
+    await expect(page).toHaveURL('/profile')
+    await expect(
+        page.locator('header')
+            .getByRole('button', { name: /log out|sign out/i })
+    ).toBeVisible();
+  })
+
+
+
 
 })
