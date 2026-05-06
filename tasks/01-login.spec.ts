4b467182a508682b72b6d847ea544729e383ce22
sebadabrowski95 <sebadabrowski95@gmail.com>
2026-05-06 18:42:02 +0200

    test(01-login): add test with negative scenarios
    Includes validation for invalid email format and empty fields.

--- tasks/01-login.spec.ts
+++ tasks/01-login.spec.ts
@@ -15,24 +15,56 @@ test.describe('Login', () => {
     await page.goto('/login')
   })
 
-  test('logs in with valid credentials', async ({ page }) => {
-    await expect(
-        page.locator('header')
-            .getByRole('button', { name: /log out|sign out/i })
-    ).not.toBeVisible();
+    test('logs in with valid credentials and verifies if the user is actually logged in.', async ({ page }) => {
+        await expect(
+            page.locator('header')
+                .getByRole('button', { name: /log out|sign out/i })
+        ).not.toBeVisible();
 
-    await page.getByLabel('Email Address').fill(VALID_EMAIL)
-    await page.getByLabel('Password').fill(VALID_PASSWORD)
-    await page.click('[type="submit"]')
+        await page.getByLabel('Email Address').fill(VALID_EMAIL)
+        await page.getByLabel('Password').fill(VALID_PASSWORD)
+        await page.click('[type="submit"]')
 
-    await expect(page).toHaveURL('/profile')
-    await expect(
-        page.locator('header')
-            .getByRole('button', { name: /log out|sign out/i })
-    ).toBeVisible();
-  })
+        await expect(page).toHaveURL('/profile')
+        await expect(
+            page.locator('header')
+                .getByRole('button', { name: /log out|sign out/i })
+        ).toBeVisible();
+    })
 
+    test('shows an error for invalid email and password', async ({ page }) => {
+        await page.fill('#email', 'wrong@example.com')
+        await page.fill('#password', 'wrongpassword')
+        await page.click('[type="submit"]')
 
+        await expect(page).toHaveURL(/\/login$/)
+        await expect(page.getByText(/login failed/i)).toBeVisible() 
+    })
+
+    test('shows an error if logs in with invalid email format', async ({ page }) => {
+        await page.fill('#email', 'wrong')
+        await page.fill('#password', 'wrongpassword')
+        await page.click('[type="submit"]')
+
+        await expect(page).toHaveURL(/\/login$/)
+        await expect(page.getByText(/login failed/i)).toBeVisible() 
+    })
+
+    test('shows an error if password is missing', async ({ page }) => {
+        await page.fill('#email', 'wrong@exampl')
+        await page.click('[type="submit"]')
+
+        await expect(page).toHaveURL(/\/login$/)
+        await expect(page.getByText(/Password is required/i)).toBeVisible() 
+    })
+
+    test('shows an error if email is missing', async ({ page }) => {
+        await page.fill('#password', 'VALID_PASSWORD')
+        await page.click('[type="submit"]')
+
+        await expect(page).toHaveURL(/\/login$/)
+        await expect(page.getByText(/Email is required/i)).toBeVisible() 
+    })
 
 
 })
